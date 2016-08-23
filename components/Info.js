1d7245110b215b74f24d3444c4a7ca2df3d9db66
Henrik <henrik@unswornindustries.com>
2016-08-23 18:32:29 +0200

    Better Info

--- components/Info.js
+++ components/Info.js
@@ -5,7 +5,12 @@ export default class Info extends React.Component {
   render() {
     return (
       <div className="info">
-        {this.props.entry && this.props.entry.name}
+        {this.props.entry &&
+          <div>
+            <div className="info-name">{this.props.entry.Namn}</div>
+            <div className="info-price">{this.props.entry.Prisinklmoms} kr inklusive moms</div>
+          </div>
+        }
       </div>
     );
   }
